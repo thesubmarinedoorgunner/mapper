bc1a43d6a6601bae7a0cbb3f5490e4ffb49d271c
Howard Cheung <howardcheung@USF-Gold-Wifi-226-22-220.laptops.usf.edu>
2018-11-05 18:13:52 -0500

    Commented out and rewriting

--- src/usf/saav/howard/rewrite/Dijkstra.java
+++ src/usf/saav/howard/rewrite/Dijkstra.java
@@ -34,6 +34,8 @@ public class Dijkstra {
 //		this.base = null;	
 //	}
 	
+	public HashMap<Node, DijkstraContainer> container;
+	
 	public Dijkstra(ArrayList<Node> nodes, ArrayList<Edge> edges, Node base)
 	{
 		this.nodes = nodes;
@@ -42,15 +44,17 @@ public class Dijkstra {
 		//this.PQ = new PriorityQueue<Node>(nodes.size(), new compare() );
 		//this.dist = new int[ nodes.size() ];
 		this.queue = new ArrayList<Node>(this.nodes);
-		System.out.println("Queue was set. Passed all assignments.");
-		setInfinity();
-		System.out.println("Infinity was set.");
-		setNull();
-		System.out.println("Null was set");
-		calculateDijkstra();
-		System.out.println("Dijkstra was calculated.");
-		setPath();
-		System.out.println("Path was set.");
+//		System.out.println("Queue was set. Passed all assignments.");
+//		setInfinity();
+//		System.out.println("Infinity was set.");
+//		setNull();
+//		System.out.println("Null was set");
+//		calculateDijkstra();
+//		System.out.println("Dijkstra was calculated.");
+//		setPath();
+//		System.out.println("Path was set.");
+		
+		this.container = new HashMap<Node, DijkstraContainer>();
 
 	}
 	
@@ -61,160 +65,160 @@ public class Dijkstra {
 	//public void setPQ(ArrayList<Node> nodes, ArrayList<Edge> edges, Node base) { this.PQ = new PriorityQueue(nodes.size(), new compare() ); }
 	
 	
-	public void setInfinity() 
-	{
-		for (int i = 0; i < nodes.size(); i++)
-		{
-			if (nodes.get(i) == base)
-			{
-				dist.put(nodes.get(i), 0);
-				continue;
-			}
-			this.dist.put(nodes.get(i), Integer.MAX_VALUE);
-		}
-	}
-	
-	public void setNull()
-	{
-		for (int i = 0; i < nodes.size(); i++)
-		{
-			this.previous.put(nodes.get(i), null);
-		}
-	}
-	
-	public void calculateDijkstra()
-	{
-		
-		while ( !queue.isEmpty() )
-		{
-			Node u = findMin();
-			if ( u == null ) { return; }
-			//System.out.println("But we're still here ?");
-			System.out.println( queue.size() );
-			ArrayList<Edge> neighbors = getNeighbors( u );
-			
-//			if ( neighbors.size() == 0 )
-//			{
-//				queue.remove(u);
-//				continue;
-//			}
-			
-			
-			for (Edge edge : neighbors)
-			{
-				Node v = null;
-				if ( edge.getp0() == u ) { v = edge.getp1(); }
-				else if ( edge.getp1() == u) { v = edge.getp0(); }
-
-				float alt = Integer.valueOf( dist.get( u ) ) + edge.getEuclideanDistance();
-				
-				if (alt < Integer.valueOf( dist.get(v)) )
-				{
-					alt = Math.round(alt);
-					//dist.put(v, (int) alt);
-					dist.replace(v, dist.get(v), (int)alt);
-					previous.replace(v, previous.get(v), u);
-				}
-			}
-			
-			neighbors.clear();
-			queue.remove(u);
-		}
-	}
-	
-	
-	public Node findMin()
-	{
-		int min = Integer.MAX_VALUE;
-		Node isMin = null;
-		for (int i = 0; i < queue.size(); i++)
-		{
-			//if ( Integer.valueOf( dist.get( queue.get(i) ) ) == Integer.MAX_VALUE) { continue; }
-			if ( Integer.valueOf( dist.get( queue.get(i) ) ) < min )
-			{
-				min = Integer.valueOf( dist.get( queue.get(i) ) );
-				isMin = queue.get(i);
-			}
-		}
-		
-		//if ( min == Integer.MAX_VALUE) { return queue.get(0); }
-		
-		return isMin;
-	}
-	
-	public ArrayList<Edge> getNeighbors(Node u)
-	{
-		ArrayList<Edge> neighbors = new ArrayList<Edge>();
-		
-		for (Edge edge : edges)
-		{
-			if ( edge.getp0() == u && queue.contains(edge.getp1() ))
-			{	
-				neighbors.add(edge);
-//				for (Node node : queue)
-//				{
-//					if ( queue.contains(edge.getp1()) ) { neighbors.add(edge); }
-//				}
-//				//else
-//				continue;
-			}
-			if ( edge.getp1() == u && queue.contains(edge.getp0() ))
-			{
-				neighbors.add(edge);
-//				for (Node node : queue)
-//				{
-//					if ( queue.contains(edge.getp0()) ) { neighbors.add(edge); }
-//				}
-//				// else
-//				continue;
-			}
-		}
-		return neighbors;
-	}
-	
-	public void setPath()
-	{
-//		Iterator iterate = previous.entrySet().iterator();
-//		while ( iterate.hasNext() )
+//	public void setInfinity() 
+//	{
+//		for (int i = 0; i < nodes.size(); i++)
 //		{
-//			HashMap.Entry pair = (HashMap.Entry)iterate.next();
-//			
-//			if ( pair.getValue() == null )
+//			if (nodes.get(i) == base)
 //			{
-//				iterate.remove(); // avoids a ConcurrentModificationException
+//				dist.put(nodes.get(i), 0);
 //				continue;
 //			}
-//			// I think the problem is when I cast
-//			path.add( findEdge( (Node)pair.getKey(), (Node)pair.getValue()) );
-//			
+//			this.dist.put(nodes.get(i), Integer.MAX_VALUE);
 //		}
-		
-		// this gives me the same problem
-		for (Node key : previous.keySet() )
-		{
-			if ( previous.get(key) != null ) 
-			{
-				path.add( findEdge( key, previous.get(key) )) ;
-			}
-		}
-	}
+//	}
+//	
+//	public void setNull()
+//	{
+//		for (int i = 0; i < nodes.size(); i++)
+//		{
+//			this.previous.put(nodes.get(i), null);
+//		}
+//	}
+//	
+//	public void calculateDijkstra()
+//	{
+//		
+//		while ( !queue.isEmpty() )
+//		{
+//			Node u = findMin();
+//			if ( u == null ) { return; }
+//			//System.out.println("But we're still here ?");
+//			System.out.println( queue.size() );
+//			ArrayList<Edge> neighbors = getNeighbors( u );
+//			
+////			if ( neighbors.size() == 0 )
+////			{
+////				queue.remove(u);
+////				continue;
+////			}
+//			
+//			
+//			for (Edge edge : neighbors)
+//			{
+//				Node v = null;
+//				if ( edge.getp0() == u ) { v = edge.getp1(); }
+//				else if ( edge.getp1() == u) { v = edge.getp0(); }
+//
+//				float alt = Integer.valueOf( dist.get( u ) ) + edge.getEuclideanDistance();
+//				
+//				if (alt < Integer.valueOf( dist.get(v)) )
+//				{
+//					alt = Math.round(alt);
+//					//dist.put(v, (int) alt);
+//					dist.replace(v, dist.get(v), (int)alt);
+//					previous.replace(v, previous.get(v), u);
+//				}
+//			}
+//			
+//			neighbors.clear();
+//			queue.remove(u);
+//		}
+//	}
+//	
 	
-	public Edge findEdge(Node u, Node v)
-	{
-		Edge found = null;
-		
-		for (Edge edge : edges)
-		{
-			if ( (u == edge.getp0() && v == edge.getp1()) || (u == edge.getp1() && v == edge.getp0()) );
-			{
-				found = edge;
-				break;
-			}
-		}
-		
-		return found;
-	}
+//	public Node findMin()
+//	{
+//		int min = Integer.MAX_VALUE;
+//		Node isMin = null;
+//		for (int i = 0; i < queue.size(); i++)
+//		{
+//			//if ( Integer.valueOf( dist.get( queue.get(i) ) ) == Integer.MAX_VALUE) { continue; }
+//			if ( Integer.valueOf( dist.get( queue.get(i) ) ) < min )
+//			{
+//				min = Integer.valueOf( dist.get( queue.get(i) ) );
+//				isMin = queue.get(i);
+//			}
+//		}
+//		
+//		//if ( min == Integer.MAX_VALUE) { return queue.get(0); }
+//		
+//		return isMin;
+//	}
 	
+//	public ArrayList<Edge> getNeighbors(Node u)
+//	{
+//		ArrayList<Edge> neighbors = new ArrayList<Edge>();
+//		
+//		for (Edge edge : edges)
+//		{
+//			if ( edge.getp0() == u && queue.contains(edge.getp1() ))
+//			{	
+//				neighbors.add(edge);
+////				for (Node node : queue)
+////				{
+////					if ( queue.contains(edge.getp1()) ) { neighbors.add(edge); }
+////				}
+////				//else
+////				continue;
+//			}
+//			if ( edge.getp1() == u && queue.contains(edge.getp0() ))
+//			{
+//				neighbors.add(edge);
+////				for (Node node : queue)
+////				{
+////					if ( queue.contains(edge.getp0()) ) { neighbors.add(edge); }
+////				}
+////				// else
+////				continue;
+//			}
+//		}
+//		return neighbors;
+//	}
+	
+//	public void setPath()
+//	{
+////		Iterator iterate = previous.entrySet().iterator();
+////		while ( iterate.hasNext() )
+////		{
+////			HashMap.Entry pair = (HashMap.Entry)iterate.next();
+////			
+////			if ( pair.getValue() == null )
+////			{
+////				iterate.remove(); // avoids a ConcurrentModificationException
+////				continue;
+////			}
+////			// I think the problem is when I cast
+////			path.add( findEdge( (Node)pair.getKey(), (Node)pair.getValue()) );
+////			
+////		}
+//		
+//		// this gives me the same problem
+//		for (Node key : previous.keySet() )
+//		{
+//			if ( previous.get(key) != null ) 
+//			{
+//				path.add( findEdge( key, previous.get(key) )) ;
+//			}
+//		}
+//	}
+	
+//	public Edge findEdge(Node u, Node v)
+//	{
+//		Edge found = null;
+//		
+//		for (Edge edge : edges)
+//		{
+//			if ( (u == edge.getp0() && v == edge.getp1()) || (u == edge.getp1() && v == edge.getp0()) );
+//			{
+//				found = edge;
+//				break;
+//			}
+//		}
+//		
+//		return found;
+//	}
+//	
 	
 	
 	
@@ -273,11 +277,11 @@ public class Dijkstra {
 	
 	*/
 	
-	public class DijkstraCalculation
+	public class DijkstraContainer
 	{
 		private float dist;
 		private Node prev;
-		DijkstraCalculation()
+		DijkstraContainer()
 		{
 			this.dist = Float.MAX_VALUE;
 			this.prev = null;
